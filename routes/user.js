4b7169fc6ec00fce92289db6a9ef10ae49e33a98
PazAutumn <pazcontrerastello@gmail.com>
2018-06-07 16:53:43 -0400

    Sección 4 clase 22

--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,6 @@ var md_auth = require('../middlewares/authenticated');
 api.get('/probando-controlador', md_auth.ensureAuth, userController.pruebas);
 api.post('/register', userController.saveUser);
 api.post('/login', userController.loginUser);
+api.put('/update-user/:id', md_auth.ensureAuth, userController.updateUser);
 
 module.exports = api;
