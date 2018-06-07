74153fa6d2188717e8d232192bfbf6bcb502e7aa
PazAutumn <pazcontrerastello@gmail.com>
2018-06-07 11:49:38 -0400

    Sección 4 clase 18

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,54 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
+var newUser = require('../models/user');
+
 function pruebas(req, res){
 	res.status(200).send({
 		message: 'Probando una acción del controlador de usuarios del api rest con node y mongo'
 	});
 }
 
+function saveUser(req, res){
+	var user = new newUser();
+
+	var params = req.body;
+
+	console.log(params);
+
+	user.name = params.name;
+	user.surname = params.surname;
+	user.email = params.email;
+	user.role = 'ROLE_ADMIN';
+	user.image = 'null';
+
+	if(params.password){
+		//Encriptar contraseña y guardar datos
+		bcrypt.hash(params.password, null, null, function(err, hash){
+			user.password = hash;
+			if(user.name != null && user.surname != null && user.email != null){
+				//Guardar usuario
+				user.save((err, userStored) => {
+					if(err){
+						res.status(500).send({message: 'Error al gurdar usuario'});
+					}else{
+						if(!userStored){
+							res.status(404).send({message: 'No se ha registrado el usuario'});
+						}else{
+							res.status(200).send({user: userStored});
+						}
+					}
+				});
+			}else{
+				res.status(200).send({message: 'Rellena todos los campos'});
+			}
+		});
+	}else{
+		res.status(200).send({message: 'Introduce contraseña'});
+	}
+}
+
 module.exports = {
-	pruebas
+	pruebas,
+	saveUser
 };
--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ var userController = require('../controllers/user');
 var api = express.Router();
 
 api.get('/probando-controlador', userController.pruebas);
+api.post('/register', userController.saveUser);
 
 module.exports = api;
